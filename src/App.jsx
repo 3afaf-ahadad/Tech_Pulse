e95b1111249441cf0129a74958d525e4b3c58ef6
Afaf <afaf20ahadad@gmail.com>
2026-03-28 22:02:27 +0000

    exp

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import './index.css'
-
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-
   return (
-    <>
-      Tech Pulse
-    </>
-  )
+    <div className="min-h-screen bg-luxury-com-dark text-gray-300 font-sans flex">
+      <h1>Tech Pulse</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
