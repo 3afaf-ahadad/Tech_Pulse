233f4dc50a99931fe6fa8b5ec27ef98814c2cd36
Afaf <afaf20ahadad@gmail.com>
2026-04-10 18:53:34 +0000

    feat: complete redux store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { techNewsApi } from "./services/techNewsApi";
-
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
-    reducer : {
-        [techNewsApi.reducerPath] : techNewsApi.reducer,
-    },
-    middleware : (getDefaultMiddleware) => get
-})
+  reducer: {
+    [techNewsApi.reducerPath]: techNewsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(techNewsApi.middleware),
+});
+setupListeners(store.dispatch);
