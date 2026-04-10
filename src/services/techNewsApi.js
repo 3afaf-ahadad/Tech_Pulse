a1c5706a5601a15d4935fc105bbe9d0d8c771adc
Afaf <afaf20ahadad@gmail.com>
2026-04-10 20:21:31 +0000

    feat: added comments

--- src/services/techNewsApi.js
+++ src/services/techNewsApi.js
@@ -2,11 +2,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 export const techNewsApi = createApi({
   reducerPath: "techNewsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://dev.to/api/" }),
+  //  l'URL Basic du API
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://dev.to/api/",
+  }),
+  // les URL specifique pour chaque page/élement dans l'API
   endpoints: (builder) => ({
     getRecentTechNews: builder.query({
-      query: () => "articles?tag=technology&per_page=10",
+      // method GET
+      query: () => "articles?tag=technology&per_page=10", // l'URL du endpoint
+      
     }),
   }),
 });
+
+// une hook auto-géneré par RTK 
 export const { useGetRecentTechNewsQuery } = techNewsApi;
