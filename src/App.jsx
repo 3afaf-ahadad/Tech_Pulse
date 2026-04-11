9a8d36244c1ecdc38f82f4fa7c1b1e6a48a120de
Afaf <afaf20ahadad@gmail.com>
2026-04-11 17:07:02 +0000

    feat: basic display , eventCard Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="min-h-screen bg-luxury-com-dark text-gray-300 font-sans flex">
+    <div>
       <h1>Tech Pulse</h1>
+      <br />
       <EventCard />
     </div>
   );
--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -8,5 +8,32 @@ export default function EventCard() {
   if (error) {
     return <p>There was an Error 😿</p>;
   }
-  console.log(data);
+
+  // console.log("Description : ", data[2].description)
+  return (
+    <div>
+      {data.map((post) => (
+        <article  key={post.id}>
+          <h2>
+            {" "}
+            <hr /> {post.title} {console.log("Description : ", post.description)}
+          </h2>
+          <img src={post.social_image} alt="image" />
+          <br />
+          <div className="tags">
+            {post.tag_list.map((tag) => (
+              <span>{tag} </span>
+            ))}
+          </div>
+          <br />
+          <br />
+          <footer>
+            Author: {post.user.username}
+            <br />
+            Published : {post.readable_publish_date}
+          </footer>
+        </article>
+      ))}
+    </div>
+  );
 }
